d0c1801dcc3dbffb12d64c2c977078cd685aa249
Eric Nantz <thercast@gmail.com>
2022-05-28 03:37:24 +0000

    quick image viewer

--- apps/art_viewer/app.R
+++ apps/art_viewer/app.R
@@ -1,25 +1,48 @@
 library(shiny)
+library(dplyr)
+
+art_sub <- readRDS("data/art_sub.rds")
 
 ui <- fluidPage(
-  sidebarLayout(
-    sidebarPanel(
-      sliderInput(
-        "obs",
-        "Number of observations:",
-        min = 10,
-        max = 500,
-        value = 100
-      ),
-      counterButton("counter1", "Counter #1")
-    ),
-    mainPanel(plotOutput("distPlot"))
+  fluidRow(
+    div(
+      align = "center",
+      uiOutput("img"),
+      actionButton(
+        "new_image",
+        "New Image"
+      )
+    )
   )
 )
 
 server <- function(input, output) {
-  callModule(counter, "counter1")
-  output$distPlot <- renderPlot({
-    hist(rnorm(input$obs), col = 'darkgray', border = 'white')
+  # get first image
+  first_image <- art_sub %>%
+    slice_sample(n = 1) %>%
+    pull(image_url)
+
+  img_exclude <- reactiveVal(NULL)
+  img_current <- reactiveVal(NULL)
+
+  observeEvent(input$new_image, {
+    img_exclude(c(img_exclude(), img_current()))
+  })
+
+  output$img <- renderUI({
+    # select image 
+    if (is.null(img_exclude())) {
+      img_current(first_image)
+    } else {
+      img <- art_sub %>%
+        filter(!image_url %in% img_exclude()) %>%
+        slice_sample(n = 1) %>%
+        pull(image_url)
+
+      img_current(img)
+    }
+
+    tags$img(src = img_current())
   })
 }
 
